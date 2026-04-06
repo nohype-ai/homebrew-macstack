cb82575bc48363ca996c4545e0113c15e1216415
codeface-io <sebastian@nohype.ai>
2026-04-07 00:06:37 +0200

    Test Homebrew tap publishing process

---
+++ Formula/macstack.rb
@@ -0,0 +1,29 @@
+class Macstack < Formula
+  desc "Tech stack management based on a personal stack definition"
+  homepage "https://macstack.dev"
+  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c0bbfcb2be1be4b1c1668f7c76623bba481c8ddd73a7f79d4305045b6e837021"
+  license "MIT"
+
+  depends_on "jq"
+  depends_on "moreutils"
+  depends_on "check-jsonschema"
+
+  def install
+    prefix.install "bin"
+    prefix.install "scripts"
+  end
+
+  def caveats
+    <<~EOS
+      To update (set up) your Mac, run:
+        mack update
+
+      On first run, mack will ask where your stack definition folder is.
+    EOS
+  end
+
+  test do
+    assert_match "Valid calls are", shell_output("#{bin}/mack help")
+  end
+end
