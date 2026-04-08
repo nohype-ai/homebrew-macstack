29015fc72932366906cf169bfa0be85d47eca014
codeface-io <sebastian@nohype.ai>
2026-04-08 16:43:32 +0200

    Add formula template for automated releases

---
+++ Formula/macstack_template.rb
@@ -0,0 +1,29 @@
+class Macstack < Formula
+  desc "Tech stack management based on a personal stack definition"
+  homepage "https://macstack.dev"
+  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/<VERSION-PLACEHOLDER>.tar.gz"
+  sha256 "<SHA256-PLACEHOLDER>"
+  license "MIT"
+
+  depends_on "jq"
+  depends_on "moreutils"
+  depends_on "check-jsonschema"
+
+  def install
+    prefix.install "bin"
+    prefix.install "scripts"
+  end
+
+  def caveats
+    <<~EOS
+      To update (set up) your Mac, run:
+        mack update
+
+      On first run, mack will ask where your stack definition folder is.
+    EOS
+  end
+
+  test do
+    assert_match "Valid calls are", shell_output("#{bin}/mack help")
+  end
+end
