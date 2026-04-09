c542f6cf4d4ec262faa054141615fc988d903e48
codeface-io <sebastian@nohype.ai>
2026-04-09 13:37:20 +0200

    Bump macstack to v0.2.1

--- Formula/macstack.rb
+++ Formula/macstack.rb
@@ -1,8 +1,8 @@
 class Macstack < Formula
   desc "Tech stack management based on a personal stack definition"
   homepage "https://macstack.dev"
-  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "372d803bdd33cf0a2c279930643fcc4d16a0497ee9666b6afd632088a51435ef"
+  url "https://github.com/nohype-ai/MacStack/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "869ba7359d108cbba43f91b663fe9f90076b6baeaccbadf925f8d4887121784d"
   license "MIT"
 
   depends_on "jq"
